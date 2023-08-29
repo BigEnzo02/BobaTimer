eaeeed184508fefcee8ca1b97696d0f95e6d9b5b
BagelCatGames <bagelcatgames@gmail.com>
2023-08-29 14:14:19 -0700

    added text and button for a timer

---
+++ app/src/main/java/com/example/bobatimer/Timer.java
@@ -0,0 +1,15 @@
+package com.example.bobatimer;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class Timer {
+
+    public void StartTimer(){
+        System.out.println("timer start");
+        //ImageButton startTimerButton = android.view.view (ImageButton) Find= "@+id/button";
+        //View.OnClickListener  =
+    }
+
+}
